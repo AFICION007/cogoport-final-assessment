f01d3654ffa1372207d7b70dd62c608c0e77d856
Abhinav Tripathi <avinav0110@gmail.com>
2023-08-04 14:14:39 +0530

    register page & login page connected to API

---
+++ src/components/RegisterForm.jsx
@@ -0,0 +1,125 @@
+import React, { useRef } from "react";
+import "./css/Form.css";
+
+const Form = () => {
+  const nameRef = useRef(null);
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const confirmPasswordRef = useRef(null);
+
+  const handleRegister = async (name, email, password, confirmPassword) => {
+    const response = await fetch("http://127.0.0.1:3001/add_user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user_name: name,
+        email,
+        password,
+        confirm_password: confirmPassword,
+        number_of_followers: 0,
+      }),
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const name = nameRef.current.value;
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    const confirmPassword = confirmPasswordRef.current.value;
+
+    if (password !== confirmPassword) {
+      alert("Your password does not match");
+      passwordRef.current.value = "";
+      confirmPasswordRef.current.value = "";
+      return;
+    }
+
+    try {
+      const response = await handleRegister(
+        name,
+        email,
+        password,
+        confirmPassword
+      );
+      if (response.status === 200) {
+        alert("User registered successfully!");
+        // Optionally, you can redirect to a new page or show a success message.
+      } else {
+        alert("Failed to register user.");
+      }
+    } catch (error) {
+      alert("Error registering user:", error.message);
+    }
+
+    event.target.reset();
+  };
+  return (
+    <div className="form-container">
+      <h2 className="form-heading">Create Account</h2>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input
+            ref={nameRef}
+            id="name"
+            type="text"
+            placeholder="John Jacobs"
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Your email
+          </label>
+          <input
+            ref={emailRef}
+            id="email"
+            type="email"
+            placeholder="name@example.com"
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            ref={passwordRef}
+            id="password"
+            type="password"
+            placeholder="password"
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="confirmPassword" className="form-label">
+            Confirm Password
+          </label>
+          <input
+            ref={confirmPasswordRef}
+            id="confirmPassword"
+            type="text"
+            placeholder="Confirm password"
+            className="form-control"
+            required
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Register
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
---
+++ src/components/SignInForm.jsx
@@ -0,0 +1,86 @@
+import React, { useRef } from "react";
+import "./css/Form.css";
+
+const SignInForm = () => {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const handleSignIn = async (email, password) => {
+    try {
+      const response = await fetch("http://127.0.0.1:3001/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        // Store the authentication token from the response
+        localStorage.setItem("authToken", data.auth_token);
+        alert(
+          `You're successfully signed in, your authtoken is ${data.auth_token}`
+        );
+
+        // update frontend state to indicate user is authenticated
+      } else {
+        const errorData = await response.json();
+        console.error("Sign-in error:", errorData.error);
+      }
+    } catch (error) {
+      console.error("Sign-in error:", error);
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    handleSignIn(email, password);
+
+    console.log("Sign-in credentials:", { email, password });
+    event.target.reset();
+  };
+
+  return (
+    <div className="form-container">
+      <h2 className="form-heading">Sign In</h2>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Your email
+          </label>
+          <input
+            ref={emailRef}
+            id="email"
+            type="email"
+            placeholder="name@example.com"
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            ref={passwordRef}
+            id="password"
+            type="password"
+            placeholder="password"
+            className="form-control"
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Sign In
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SignInForm;
